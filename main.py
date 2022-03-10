3d0c91e71db2e589f312477677ad9ca0c21f2e8a
ykyk21222 <andrewrules903@gmail.com>
2022-03-10 17:00:50 -0500

    added output box for ui

--- main.py
+++ main.py
@@ -38,4 +38,9 @@ url_entry.pack(side=tk.LEFT)
 frame = tk.Frame(root,  bg="white")  # change frame color
 frame.pack()
 
+
+# Adds an output box to GUI.
+command_textbox = tksc.ScrolledText(frame, height=10, width=100)
+command_textbox.pack()
+
 root.mainloop()
