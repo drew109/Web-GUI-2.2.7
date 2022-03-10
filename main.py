1684fe99067989142bebbfcdd0d189cc09f867f6
ykyk21222 <88405575+ykyk21222@users.noreply.github.com>
2022-03-10 16:52:55 -0500

    Added entry box and make it look decent

--- main.py
+++ main.py
@@ -18,4 +18,24 @@ frame.pack()
 ping_btn = tk.Button(frame, text="ping", command=do_command)
 ping_btn.pack()
 
+# creates the frame with label for the text box
+frame_URL = tk.Frame(root, pady=10,  bg="lime green")  # change frame color
+frame_URL.pack()
+
+# decorative label
+url_label = tk.Label(frame_URL, text="Enter a URL of interest: ",
+                     compound="center",
+                     font=("comic sans", 14),
+                     bd=0,
+                     relief=tk.FLAT,
+                     cursor="heart",
+                     fg="white",
+                     bg="black")
+url_label.pack(side=tk.LEFT)
+url_entry = tk.Entry(frame_URL,  font=("comic sans", 14))  # change font
+url_entry.pack(side=tk.LEFT)
+
+frame = tk.Frame(root,  bg="white")  # change frame color
+frame.pack()
+
 root.mainloop()
